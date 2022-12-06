bc73c9a9dcb68841ff34a2d42dd8ec1811da6522
Jake Stumph <stumph.jake@gmail.com>
2022-12-05 18:59:36 -0800

    Multiple dice test functionality

--- app/src/main/java/com/jakestumph/diceroller/MainActivity.kt
+++ app/src/main/java/com/jakestumph/diceroller/MainActivity.kt
@@ -23,8 +23,11 @@ class MainActivity : AppCompatActivity() {
         // create new dice object w six sides
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll()
         //find the imageview in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
         // determine the drawable resource ID to use based on the dice roll
         val drawableResource = when(diceRoll){
             1 -> R.drawable.dice_1
@@ -34,10 +37,22 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
+        val drawableResource2 = when(diceRoll2){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         // update the Imageview with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
         //Update the content description
         diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+
     }
 }
 
