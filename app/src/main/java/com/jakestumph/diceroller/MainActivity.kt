0ff82809ec94000ef6dcf3464e2a2ae81468479d
Jake Stumph <stumph.jake@gmail.com>
2022-12-05 18:46:57 -0800

    Commit2

--- app/src/main/java/com/jakestumph/diceroller/MainActivity.kt
+++ app/src/main/java/com/jakestumph/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jakestumph.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,17 +16,28 @@ class MainActivity : AppCompatActivity() {
 //            toast.show()
             rollDice()
         }
+        rollDice()
     }
 
     private fun rollDice() {
+        // create new dice object w six sides
         val dice = Dice(6)
-        val dice2 = Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice.roll()
-        val rollResultTextView: TextView = findViewById(R.id.rollResultText)
-        val rollResultTextView2: TextView = findViewById(R.id.rollResultText2)
-        rollResultTextView.text = diceRoll.toString()
-        rollResultTextView2.text = diceRoll2.toString()
+        //find the imageview in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // determine the drawable resource ID to use based on the dice roll
+        val drawableResource = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // update the Imageview with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        //Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
